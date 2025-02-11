9d6ddccd0184b734986a0740d1d9979f6c035b90
mandu-kim <goghinarles@gmail.com>
2025-02-10 20:48:33 -0800

    feat: add show_menu() function for displaying CLI menu

---
+++ app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+records = []
+
+def show_menu():
+    print("================================")
+    print("1) Add Record")
+    print("2) Show Record")
+    print("3) Show Stats")
+    print("================================")
+
+
