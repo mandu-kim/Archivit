2e47efc2dea9322f81bb8177154fea5ec1cf0d16
mandu-kim <goghinarles@gmail.com>
2025-02-10 22:17:30 -0800

    feat: add main function for user selection

--- app.py
+++ app.py
@@ -11,4 +11,21 @@ def show_menu():
     print("4) Exit")
     print("================================")
 
+def main():
+    while True:
+        show_menu()
+        choice = input("Select an option: ")
+        if choice == "1":
+            print("기록 추가 함수 작성 필요")
+        elif choice == "2":
+            print("기록 출력 함수 작성 필요")
+        elif choice == "3":
+            print("통계 출력 함수 작성 필요")
+        elif choice == "4":
+            print("Program is shutting down.")
+            break
+        else:
+            print("Invalid input. Please try again.\n")
 
+if __name__ == "__main__":
+    main()
