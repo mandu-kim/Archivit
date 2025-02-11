5fe70afcfe8315a949e471eb744bf3d96dd897e7
mandu-kim <goghinarles@gmail.com>
2025-02-10 22:15:39 -0800

    fix: add missing Exit option in show_menu

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ records = []
 def show_menu():
     print("================================")
     print("1) Add Record")
-    print("2) Show Record")
+    print("2) Show Records")
     print("3) Show Stats")
+    print("4) Exit")
     print("================================")
 
 
