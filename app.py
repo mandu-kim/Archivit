9a1e6566ec2751b787b338f5b10bafc6fc52f10e
mandu-kim <goghinarles@gmail.com>
2025-02-12 21:31:47 -0800

    feat: add add_record function to store new data with selectable item types

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -10,6 +12,38 @@ def show_menu():
     print("3) Show Stats")
     print("4) Exit")
     print("================================")
+
+def add_record():
+    ValueInfo = namedtuple("ValueInfo", ["label", "type_func"])
+    value_type = {
+        "a": ValueInfo("Number", int),
+        "b": ValueInfo("Text", str),
+        "c": ValueInfo("List", list)
+    }
+    print("\n[Add Record]")
+    item_name = input("Item name: ")
+    item_type = ""
+    data_type = None
+    while True:
+        print("Value Types")
+        print("\n".join(f"    {key}) {info.label}" for key, info in value_type.items())
+        choice = input("Select a type of value: ")
+        if choice in value_type:
+            item_type = value_type[choice].label
+            data_type = value_type[choice].type_func
+            break
+        else:
+            print("Invalid type. Please try again.\n")
+    raw_input = input("Value: ")
+    print("입력값 자료형이 선택한 value type 과 일치하는지 확인하기")
+    # value = validate_item_type(raw_input, data_type)
+    record = {
+        "item_name": item_name,
+        "item_type": item_type,
+        # "value": value
+    }
+    records.append(record)
+    print("=>Record has been added.\n")
 
 def main():
     while True:
